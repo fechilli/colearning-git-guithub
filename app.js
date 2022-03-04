42e995bc567ad5e35661c2d0a1b58fcec1adc4df
Luiz Felipe <luizfdas@outlook.com>
2022-03-03 23:10:12 -0300

    terceiro commit para ver se deu erro

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var paises = [];
-paises.push('Brasil', 'Argentina','Colombia','peru','chile', 'Equador','Bosnia','Venezuela','Bolivia','Bolivia','Uruguai','Guiana',"suiriname",'Paraguai','Guiana Francesa')
+paises.push('Brasil', 'Argentina','Colombia','peru','chile', 'Equador','Bosnia','Venezuela','Bolivia','Bolivia','Uruguai','Guiana',"suiriname",'Paraguai','Guiana Francesa', 'china')
 console.table(paises)
